2963635162c21e337111d262f50e84abcbcad728
Joshua Cachola <joshuaelliscachola@gmail.com>
2020-05-11 11:08:53 -0700

    add single file upload multer aws s3

--- routes/api/skatespots.js
+++ routes/api/skatespots.js
@@ -1,102 +1,95 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
-const multer = require("multer");
-const AWS = require("aws-sdk");
-const fs = require("fs");
 const { SkateSpots, SkateClips } = require("../../db/models");
-
+const upload = require("../../services/file-upload");
+// console.log(upload);
 const router = express.Router();
 
-const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  }
-});
+const singleUpload = upload.single("image");
 
-const upload = multer({ storage });
+// const uploadFile = (source, targetName, res) => {
+//   console.log("preparing to upload...");
+//   fs.readFile(source, (err, fileData) => {
+//     if (!err) {
+//       const putParams = {
+//         Bucket: "abdbucket",
+//         Key: "",
+//         Body: fileData
+//       };
 
-AWS.config.update({
-  accessKeyId: process.env.IAM_ACCESS_KEY,
-  secretAccessKey: process.env.IAM_SECRET,
-  region: "us-west-1"
-});
-
-const s3 = new AWS.S3();
-
-const uploadFile = (source, targetName, res) => {
-  console.log("preparing to upload...");
-  fs.readFile(source, (err, fileData) => {
-    if (!err) {
-      const putParams = {
-        Bucket: "abdbucket",
-        Key: "",
-        Body: fileData
-      };
-
-      s3.putObject(putParams, (err, data) => {
-        if (err) {
-          console.log(`Could not upload the file. Error: ${err}`);
-          return res.send({ success: false });
-        } else {
-          fs.unlink
-        }
-      });
-    }
-  });
-};
+//       s3.putObject(putParams, (err, data) => {
+//         if (err) {
+//           console.log(`Could not upload the file. Error: ${err}`);
+//           return res.send({ success: false });
+//         } else {
+//           fs.unlink
+//         }
+//       });
+//     }
+//   });
+// };
 /**
  *  GET Endpoint - /skatespots
  */
-router.get(
-  "/",
-  asyncHandler(async (_, res, next) => {
-    const skateSpots = await SkateSpots.findAll();
+// router.get(
+//   "/",
+//   asyncHandler(async (_, res, next) => {
+//     const skateSpots = await SkateSpots.findAll();
 
-    // skate spot validation
+//     // skate spot validation
 
-    res.json({ skateSpots });
-  })
-);
+//     res.json({ skateSpots });
+//   })
+// );
 
-/**
- *  POST Endpoint - /skatespots/:id/clips
- */
-router.post(
-  "/:id/clips",
-  asyncHandler(async (req, res, next) => {
-    const {
-      clipPath,
-      clipName,
-      clipCaption,
-      skaterId
-    } = req.body;
-    const id = parseInt(req.params.id, 10);
+// /**
+//  *  POST Endpoint - /skatespots/:id/clips
+//  */
+// router.post(
+//   "/:id/clips",
+//   asyncHandler(async (req, res, next) => {
+//     const {
+//       clipPath,
+//       clipName,
+//       clipCaption,
+//       skaterId
+//     } = req.body;
+//     const id = parseInt(req.params.id, 10);
 
-    // **** may need csrfProtection
+//     // **** may need csrfProtection
 
-    const clip = {
-      clipPath,
-      clipName,
-      clipCaption,
-      skaterId
-    };
+//     const clip = {
+//       clipPath,
+//       clipName,
+//       clipCaption,
+//       skaterId
+//     };
 
-    const skateClip = await SkateClips.create(clip);
+//     const skateClip = await SkateClips.create(clip);
 
-    res.status(201).json({ skateClip });
-  })
-);
+//     res.status(201).json({ skateClip });
+//   })
+// );
 
 /**
  *  POST Endpoint - /skatespots/:id/upload
  */
 router.post(
-  "/skatespots/:id/upload",
-  upload.single("demo_file"),
-  asyncHandler(async (req, res) => {
-    uploadFile(req.file.path, req.file.filename, res);
-  })
-)
+  // change to id
+  "/upload",
+  (req, res) => {
+    singleUpload(req, res, (err) => {
+      if (err) {
+        return res
+          .status(422)
+          .send({
+            errors: [{ title: "Image Upload Error", detail: err.message }],
+          });
+      }
+      console.log(req);
+      return res.json({ "imageUrl": req.file });
+    });
+  }
+);
 
 module.exports = router;
---
+++ services/file-upload.js
@@ -0,0 +1,27 @@
+const multer = require("multer");
+const aws = require("aws-sdk");
+const multerS3 = require("multer-s3");
+
+aws.config.update({
+  secretAccessKey: process.env.IAM_SECRET,
+  accessKeyId: process.env.IAM_ACCESS_KEY,
+  region: process.env.AWS_S3_REGION,
+});
+
+const s3 = new aws.S3();
+// console.log(s3);
+const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: "abd-bucket-dev",
+    acl: "public-read",
+    metadata: (req, file, cb) => {
+      cb(null, {fieldName: "TESTING_METADATA"});
+    },
+    key: (req, file, cb) => {
+      cb(null, Date.now().toString())
+    }
+  })
+})
+
+module.exports = upload;
