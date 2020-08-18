041325155c72d01e4d1adc0f783ad6269b6d4cbd
Joshua Cachola <joshuaelliscachola@gmail.com>
2020-08-18 02:08:48 -0700

    add dockerfile and fix seeder file

--- db/seeders/20200510015424-Skaters.js
+++ db/seeders/20200510015424-Skaters.js
@@ -1,20 +1,28 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert("Skaters", [{
-      firstName: "David",
-      lastName: "Phommachanh",
-      username: "crookiemonster",
-      email: "crookiemonster@gmail.com",
-      hashedPassword: "$2a$10$YEqFIcgLZXIgS63WNvUKPetcQv7eTMkBFYYyxFsVsS1HV1ltSOCH2",
-      phoneNumber: "8888888888",
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }], {});
+    return queryInterface.bulkInsert(
+      "Skaters",
+      [
+        {
+          firstName: "David",
+          lastName: "Phommachanh",
+          username: "crookiemonster",
+          email: "crookiemonster@gmail.com",
+          hashedPassword:
+            "$2a$10$YEqFIcgLZXIgS63WNvUKPetcQv7eTMkBFYYyxFsVsS1HV1ltSOCH2",
+          phoneNumber: "8888888888",
+          accountPhoto: "",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete("Skaters", null, {});
-  }
+  },
 };
