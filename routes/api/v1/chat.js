d778b1a714c96126b950923c2419ab1ef5ec3c8d
Joshua Cachola <joshuaelliscachola@gmail.com>
2020-10-02 19:15:43 -0700

    fix sending messages to specific chat room route

--- routes/api/v1/chat.js
+++ routes/api/v1/chat.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { ChatRoom } = require("../../../db/models");
+const { ChatRoom, ChatMessage } = require("../../../db/models");
 const asyncHandler = require("express-async-handler");
 const { requireAuth } = require("../../../auth");
 const router = express.Router();
 
 /**
- * Route - /api/v1/chatrooms
+ * Route - /api/v1/chat
  *    GET - get all chatrooms
  * 		POST - create a chat room
  */
@@ -35,31 +35,25 @@ router
   );
 
 /**
- * Route - /api/v1/chatrooms/messages/:chatRoomName
+ * Route - /api/v1/chat/messages/:chatRoomName
  *    GET - get messages from chat room
  */
 router.get(
   "/messages/:chatRoomName",
   requireAuth,
-  asyncHandler(async (req, res, next) => {
-    try {
-      const chatRoomName = req.params.chatRoomName;
-      const chatRooms = await ChatRoom.findAll({
-        where: {
-          name: chatRoomName,
-        },
-      });
+  asyncHandler(async (req, res) => {
+    const chatRoomName = req.params.chatRoomName;
+    const chatRooms = await ChatRoom.findAll({
+      where: { name: chatRoomName },
+    });
 
-      const chatRoomId = chatRooms[0].id;
-      const messages = models.ChatMessage.findAll({
-        where: {
-          chatRoomId,
-        },
-      });
-      res.send(messages);
-    } catch (err) {
-      res.send([]);
-    }
+    const chatRoomId = chatRooms[0].id;
+    const messages = await ChatMessage.findAll({
+      where: {
+        chatRoomId,
+      },
+    });
+    res.json(messages);
   })
 );
 
